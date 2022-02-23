272e1f9dbb41d4a8d7bfb1bd96522fb76a7d431d
eswaribala <parameswaribala@gmail.com>
2022-02-23 15:49:59 +0530

    updated

--- inventoryemapi/src/main/java/com/jd/inventoryapi/repositories/InventoryRepository.java
+++ inventoryemapi/src/main/java/com/jd/inventoryapi/repositories/InventoryRepository.java
@@ -1,12 +1,13 @@
 package com.jd.inventoryapi.repositories;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class InventoryRepository {
-    @Autowired
+    @PersistenceContext
 	private EntityManager entityManager;
 }
