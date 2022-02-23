f99e33d8375c401a73ce0055eb1fe57233b4d9ac
eswaribala <parameswaribala@gmail.com>
2022-02-23 14:50:11 +0530

    updated

--- inventoryapi/src/main/java/com/jd/inventoryapi/controllers/CategoryController.java
+++ inventoryapi/src/main/java/com/jd/inventoryapi/controllers/CategoryController.java
@@ -1,6 +1,8 @@
 package com.jd.inventoryapi.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,8 +13,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.bohnman.squiggly.Squiggly;
+import com.github.bohnman.squiggly.util.SquigglyUtils;
 import com.jd.inventoryapi.models.Category;
 import com.jd.inventoryapi.services.CategoryService;
 
@@ -53,6 +59,31 @@ public class CategoryController {
     	else
     		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Category with "
     				+ "Id"+categoryId+ "Not Deleted");
+    	
+    }
+    
+    @GetMapping({"/v1.0/filter/{categoryId}", "/v1.1/filter/{categoryId}"})
+    public ResponseEntity<?> getFilteredCategoryById(@PathVariable("categoryId") 
+    long categoryId,@RequestParam(name = "fields", required = false) String fields){
+    	
+    	Map<Object,Object> model=new HashMap<>();
+    	
+    	Category category= this.categoryService.getCategoryById(categoryId);
+    	
+    	if(category!=null)
+    	{
+    		//fields refers to runtime selection
+    		ObjectMapper mapper = Squiggly.init(new ObjectMapper(), fields);  		
+			return ResponseEntity.ok(SquigglyUtils.stringify(mapper, category));
+
+    	}
+    	else
+    	{
+	         model.put("message", "category not existing");
+		        
+	         return ResponseEntity.ok(model);
+    	}
+
     	
     }
     
